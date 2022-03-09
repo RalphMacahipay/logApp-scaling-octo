620989892d601a58de1aa68c37f32c09d33f5e6d
Ralph Kevin <kevinmacahipay7@gmail.com>
2022-03-08 18:04:49 -0700

    done

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	define('ROOT_URL', 'http://localhost//log_app/logApp-scaling-octo/index.php');
+	define('ROOT_URL', 'index.php');
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASS', '');
